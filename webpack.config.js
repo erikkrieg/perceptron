a0a95bbcb70077f02a731b419d7c3874d9612614
Erik Krieg <erik.c.krieg@gmail.com>
2017-06-13 22:49:17 -0400

    adds eslint loader and html plugin to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,38 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+function resolve (dir) {
+    return path.join(__dirname, dir)
+}
 
 module.exports = {
     entry: './src/main.js',
     output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
+        path: resolve('dist'),
+        filename: 'bundle.js'
     },
     module: {
         loaders: [
+            {
+                test: /\.js$/,
+                loader: 'eslint-loader',
+                enforce: 'pre',
+                include: [resolve('src'), resolve('test')],
+                options: {
+                    formatter: require('eslint-friendly-formatter')
+                }
+            },
             {
                 test: path.join(__dirname, 'src'),
                 loader: 'babel-loader'
             }
         ]
-    }
-};
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        })
+    ]
+}
